14020c460e5fe083c9539690a51a50bf750805da
emadupre <dupre.dev@gmail.com>
2025-08-27 20:19:52 -0300

    Soy Ema

--- src/trabajopractico2/TrabajoPractico2.java
+++ src/trabajopractico2/TrabajoPractico2.java
@@ -20,6 +20,7 @@ public class TrabajoPractico2 {
         System.out.println("que onda pibes");
         System.out.println("arranca o no arranca ");
         System.out.println("hola ^-^");
+        System.out.println("Soy Ema");
     }
     
 }
