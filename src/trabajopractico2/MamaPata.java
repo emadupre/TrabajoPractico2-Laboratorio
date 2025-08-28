4cc042fbd1834827aca3f1069733da860eb37e2d
emadupre <dupre.dev@gmail.com>
2025-08-28 17:25:17 -0300

    MamaPata class y OrniVerde

---
+++ src/trabajopractico2/MamaPata.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+package trabajopractico2;
+
+/**
+ *
+ * @author emadupre
+ */
+public interface MamaPata {
+    final double pico = 6.8;
+    final double patas = 7.5;
+    
+    void tocarOrgano();
+}
